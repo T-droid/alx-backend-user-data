6eb3c57aef3a35714a9f147b627d84c48e23673d
emmanuel tindi <emmanueltindi66@gmail.com>
2024-04-23 16:49:52 +0300

    update basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
+from models.base import Base as B
+from models.user import User as u
 
 
 class BasicAuth(Auth):
@@ -36,4 +39,52 @@ class BasicAuth(Auth):
             return (None, None)
         user_credentials = tuple(decoded_base64_authorization_header.split(':'))
         return user_credentials
-        
+    
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'): # type: ignore
+        """
+        Returns the User instance based on his
+        email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            found_users = u.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+
+        return user    
